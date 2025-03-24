2aaaa73fcb989dbe9de910128ebaf95dd3dcce4b
OsirisZhou <13330166118@163.com>
2025-03-25 00:59:23 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// 可以添加JavaScript代码来动态修改底部文字，例如：
+document.getElementById("footerText").innerText = "2025 Catsimple / It's 631th day we are in love.";
+
+// 或者，可以添加其他交互效果，例如点击事件：
+document.getElementById("myFooter").addEventListener("click", function() {
+  alert("你点击了底部文字！");
+});
