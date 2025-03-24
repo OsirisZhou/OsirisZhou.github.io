059005b6e0e4fbd464cea3022feb8d21d52c1826
OsirisZhou <13330166118@163.com>
2025-03-25 02:07:42 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const centeredTextDiv = document.createElement("div");
+  centeredTextDiv.id = "centeredText";
+  centeredTextDiv.textContent = "Osiris Loves Catismple Forever.";
+  document.body.appendChild(centeredTextDiv);
+});
