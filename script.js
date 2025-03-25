e09e3a28fffe295ce58ad3c852d27ab896b51188
OsirisZhou <13330166118@163.com>
2025-03-25 14:04:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const animation = lottie.loadAnimation({
     renderer: 'svg', // 渲染方式 (svg, canvas, html)
     loop: true, // 是否循环播放
     autoplay: true, // 是否自动播放
-    path: 'loading.json' // Lottie JSON 文件的路径 (替换为你的文件路径)
+    path: 'osiris bento/05.json' // Lottie JSON 文件的路径 (替换为你的文件路径)
 });
 
 // 模拟加载完成 (实际应用中，替换为你的加载逻辑)
