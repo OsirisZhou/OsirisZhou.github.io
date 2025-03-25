321985c7821df79733cf9bb581f38403c4dd37d8
OsirisZhou <13330166118@163.com>
2025-03-25 17:54:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const animationContainer = document.getElementById('animation-container');
+ Container = document.getElementById('animation-container');
   const audio = document.getElementById('myAudio');
   let animation; // Declare animation variable
 
   function loadAnimation() {
-    animation = lottie.loadAnimation({
+  const animation = lottie.loadAnimation({
       container: animationContainer,
-      renderer: 'canvas', // 使用 Canvas 渲染器
-      loop: true,
-      autoplay: true,
-      path: 'osiris bento/01.json', // 确保路径正确   
-    });
+      renderer: 'svg', // 渲染方式 (svg, canvas, html)
+    loop: true, // 是否循环播放
+    autoplay: true, // 是否自动播放
+    path: 'osiris bento/05.json' // Lottie JSON 文件的路径 (替换为你的文件路径)
+  });
+
+    setTimeout(function() {
+    document.getElementById('animation-container').style.display = 'none'; // 隐藏 animation-container
+  }, 2000);
+});
+
 
     animation.addEventListener('DOMLoaded', function() {
       console.log("Lottie animation loaded successfully - DOMLoaded event triggered!");
       animationContainer.style.display = 'none';
-
-      // 降低动画播放速度，延长播放时间
-      animation.setSpeed(0.5); // 设置播放速度为 0.5 倍
-
-      animation.play(); // 播放动画
-    });
-
+ }
+     
     animation.addEventListener('complete', function() {
       console.log("Lottie animation completed");
     });
