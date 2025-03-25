8cb8268f79f6524caf32a42b3105bf0e5e04eb70
OsirisZhou <13330166118@163.com>
2025-03-25 16:01:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
+  const animationContainer = document.getElementById('animation-container');
   const animation = lottie.loadAnimation({
-    container: document.getElementById('loading'), // 动画容器
+    container: animationContainer, // 动画容器
     renderer: 'svg', // 渲染方式 (svg, canvas, html)
     loop: true, // 是否循环播放
     autoplay: true, // 是否自动播放
     path: 'osiris bento/05.json' // Lottie JSON 文件的路径 (替换为你的文件路径)
   });
+  const audio = document.getElementById('myAudio');
 
-  // 模拟加载完成 (实际应用中，替换为你的加载逻辑)
-  setTimeout(function() {
-    document.getElementById('animation-container').style.display = 'none'; // 隐藏 animation-container
-  }, 2000);
+  // 监听 Lottie 动画的 loopComplete 事件 (如果需要循环播放完成后触发)
+  animation.addEventListener('loopComplete', function() {
+    animationContainer.style.display = 'none';
+    audio.play();
+  });
+
+ // 监听 Lottie 动画加载完成事件 (推荐)
+  animation.addEventListener('data_ready', function() {
+    animationContainer.style.display = 'none';
+    audio.play();
+  });
 });
+
